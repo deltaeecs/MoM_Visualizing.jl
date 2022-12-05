0367da0299d3df872458c4d2b38378ed76df7d2a
deltaeecs <1225385871@qq.com>
2022-12-05 11:18:37 +0800

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.29

---
+++ src/MoM_Visualizing.jl
@@ -0,0 +1,5 @@
+module MoM_Visualizing
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using MoM_Visualizing
+using Test
+
+@testset "MoM_Visualizing.jl" begin
+    # Write your tests here.
+end
